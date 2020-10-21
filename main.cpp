96f266b94ed33b8aa42e3240e00d0a5ea9225a5c
Cyrus Behroozi <cyrus.behr@gmail.com>
2020-10-21 10:21:55 -0700

    Write binary files to disk

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main() {
     // --------------- Configuration parameters ---------------------
     const std::string parentDir = "/home/cyrus/work/data/mugshots/";
     const std::string descriptorFilename = "unique.txt";
-    const unsigned int numTemplates = 1000;
+    const unsigned int numTemplates = 30;
     // --------------- Configuration parameters ---------------------
 
     Trueface::ConfigurationOptions options;
@@ -50,6 +50,68 @@ int main() {
     std::string token;
     int numProcessed = 0;
     int offset = 0;
+
+    int prevIdentity = -1;
+    int counter = 0;
+
+    while(std::getline(infile, line)) {
+        offset++;
+        if(offset < 10000) {
+            continue;
+        }
+
+        std::istringstream tokenStream(line);
+        std::vector<std::string> words;
+
+        while(std::getline(tokenStream, token, ',')) {
+            words.push_back(token);
+        }
+
+        std::cout << numProcessed << "/" << numTemplates << std::endl;
+        if (numProcessed++ >= numTemplates) {
+            break;
+        }
+
+        const auto path = words[1].substr(words[1].find_last_of('/'));
+        const auto imagePath = parentDir + "data" + path;
+
+        // Images are already cropped and aligned
+        auto imgBGR = cv::imread(imagePath);
+        cv::Mat imgRGB;
+        cv::cvtColor(imgBGR, imgRGB, cv::COLOR_BGR2RGB);
+
+        // Ensure the image size is correct
+        if (imgRGB.cols != 112 || imgRGB.rows != 112) {
+            std::cout << "Image at path: " << imagePath << " has incorrect size: " << imgRGB.rows << " " << imgRGB.cols << std::endl;
+            continue;
+        }
+
+        Trueface::Faceprint faceprint;
+        auto retCode = sdk.getFaceFeatureVector(imgRGB.data, faceprint);
+        if (retCode != Trueface::ErrorCode::NO_ERROR) {
+            continue;
+        }
+
+        ImageInfo imageInfo;
+        imageInfo.faceprint = std::move(faceprint);
+        imageInfo.identity = stoi(words[0]);
+        collection.emplace_back(std::move(imageInfo));
+
+        counter++;
+
+        if (prevIdentity != imageInfo.identity) {
+            prevIdentity = imageInfo.identity;
+            counter = 0;
+        }
+        // Save all the images and save the descriptor to a manifest file
+
+        std::string filename = words[0] + "_" + std::to_string(counter) + ".bin";
+        std::string writePath = "../binaryImages/" + filename;
+        std::ofstream outfile (writePath, std::ofstream::binary);
+
+        outfile.write(reinterpret_cast<const char*>(imgRGB.data), 112 * 112 * 3);
+        outfile.close();
+    }
 
     return 0;
 }
